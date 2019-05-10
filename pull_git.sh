0be5eff7cd295cb1e660736db89573a7c54715d5
Khok <hongjing.khok@lazada.com>
2019-05-10 12:13:07 +0800

    Commit from L

--- pull_git.sh
+++
@@ -1,1 +0,0 @@
-git pull https://github.com/lonedune/lonedune.github.io master
--- push_git.sh
+++
@@ -1,3 +0,0 @@
-git add -A
-git commit -m "Commit from L"
-git push https://github.com/lonedune/lonedune.github.io master
